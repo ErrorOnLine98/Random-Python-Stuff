4ecf98b3dccda74f79ce11aefdb09e6800be14e6
Adam King <adammarshallking@gmail.com>
2023-01-27 01:08:33 -0600

    Added gcd function and main function to module and added more descriptive print statements

--- greatest_common_divisor.py
+++ greatest_common_divisor.py
@@ -1,7 +1,7 @@
 #####################################################
 # Greatest Common Divisor Using Euclidean Algorithm #
 # Adam King                                         #
-# Last Updated: 5/10/2021                           #
+# Last Updated: 1/27/2022                           #
 #####################################################
 
 # The purpose of this program is to calculate the GCD of two ints using the Euclidean algorithm.
@@ -9,21 +9,36 @@
 
 import math
 
-print("This program calculates the greatest common divisor (GCD) of two integers.")
-print("It also provides a demonstration of the steps involved to get the answer.")
-print("You will provide two integers (a and b) to calculate gcd(a,b).")
 
-a = int(input("a: "))
-b = abs(int(input("b: ")))
+def gcd(a, b):
+    while b > 0:
+        # Demonstration of algorithm
+        q = math.floor(a / b)
+        print("\n")
+        print(str(a) + " = " + str(b) + " x " + str(q) + " + " + str(a % b))
 
-while b > 0:
-    # Demonstration of algorithm
-    q = math.floor(a / b)
-    print(str(a) + " = " + str(b) + " x " + str(q) + " + " + str(a % b))
+        # Implementation
+        c = b
+        b = a % b
+        a = c
 
-    # Implementation
-    c = b
-    b = a % b
-    a = c
+    return a
 
-print(str(a) + " is the gcd.")
+
+def main():
+    print("This program calculates the greatest common divisor (GCD) of two integers using the Euclidean algorithm.")
+    print("It also provides a demonstration of the steps involved to get the answer.")
+    print("You will provide two integers (a and b) to calculate gcd(a,b) = gcd(b, a % b).")
+    print("The program will then print the steps and the answer.")
+    print("Note: The program will not work if you provide a negative number for b. The program will automatically "
+          "make b positive.\n")
+    print("gcd(a,b)")
+
+    a = int(input("a: "))
+    b = abs(int(input("b: ")))
+
+    print("\n" + str(gcd(a, b)) + " is the gcd.")
+
+
+if __name__ == "__main__":
+    main()
